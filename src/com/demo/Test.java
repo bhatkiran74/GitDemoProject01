a3dd57c9fb8a4b5f28c20ab5fecaa76c7b2ab4c1
Kiran_B <bhatkiran74@gmail.com>
2023-01-13 18:00:41 +0530

    first commit

---
+++ src/com/demo/Test.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String str1 = new String("Kiran");
+		String str2 = "Kiran";
+		
+		if (str1.equals(str2)) {
+			System.out.println("True");
+		}
+
+	}
+
+}
